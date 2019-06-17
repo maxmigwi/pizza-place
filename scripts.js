987c27cf41a7e67e156484fb25bbbe9186ed1535
maxwel <maxwelmigwi@gmail.com>
2019-06-17 15:49:22 +0300

    made some changes to the js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,14 @@ function Pizza(size, toppings, crusts, delivery) {
   this.delivery = delivery;
 }
 
+Pizza.prototype.price = function() {
+  if (this.size === 'small') {var price = 600;
+  } else if (this.size === 'medium') {var price = 800;
+  } else {var price = 1000;}
+  price += this.size.length;
+  return price;
+}
+
 
 
 
